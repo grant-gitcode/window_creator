e440f13b3cd431cc4e7cadd8f27a1e835798b1be
Grant <grant.tbird@gmail.com>
2017-10-31 18:41:36 -0500

    Worked on the popup window styling a bit.

--- browser_action.js
+++ browser_action.js
@@ -1,3 +1,13 @@
+//Sets the browser action window size to 20% of the current Window size.
+document.addEventListener("DOMContentLoaded", function(event) {
+  chrome.windows.getCurrent(function(window) {
+    var bod = document.getElementById("bod");
+    bod.style.width = (window.width * .2).toString() + "px";
+  });
+
+
+});
+
 /*
 
 var urls = "";
